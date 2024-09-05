346862cd62a06502e25a4da8ad68a1426ce0aea5
MariuszSolvd <mkaminski@solvd.com>
2024-09-05 16:52:47 +0200

    Move files inside src

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi! I am just exercise");
-        System.out.println("Check build!");
+        System.out.println("Check build!!");
     }
 }
